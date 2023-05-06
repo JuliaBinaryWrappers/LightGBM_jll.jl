2532860edd209274418ee562dfe81f8b0b413442
jlbuild <juliabuildbot@gmail.com>
2023-05-06 17:57:25 +0000

    LightGBM_jll build 3.3.5+0

---
+++ src/LightGBM_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule LightGBM_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("LightGBM")
+JLLWrappers.@generate_main_file("LightGBM", UUID("0e4427ef-1ff7-5cd7-8faa-8ff0877bb2ec"))
+end  # module LightGBM_jll
