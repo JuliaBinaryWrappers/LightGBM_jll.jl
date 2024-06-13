ad34b1ce23d81443ff3ca1a5d336fb883253c914
jlbuild <juliabuildbot@gmail.com>
2024-06-13 17:22:18 +0000

    LightGBM_jll build 4.3.0+0

--- src/wrappers/armv6l-linux-gnueabihf-cxx03.jl
+++ src/wrappers/armv6l-linux-gnueabihf-cxx03.jl
@@ -2,11 +2,13 @@
 export lib_lightgbm, lightgbm
 
 using CompilerSupportLibraries_jll
+using OpenCL_jll
+using boost_jll
 JLLWrappers.@generate_wrapper_header("LightGBM")
 JLLWrappers.@declare_library_product(lib_lightgbm, "lib_lightgbm.so")
 JLLWrappers.@declare_executable_product(lightgbm)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, OpenCL_jll, boost_jll)
     JLLWrappers.@init_library_product(
         lib_lightgbm,
         "lib/lib_lightgbm.so",
